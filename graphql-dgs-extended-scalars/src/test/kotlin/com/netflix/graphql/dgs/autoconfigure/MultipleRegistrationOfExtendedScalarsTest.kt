8a486cb85b6a3b87de4b2f883221c95f6d4ffd0d
bono007 <chris.bono@gmail.com>
2021-04-17 08:56:50 -0500

    Rename packages from autoconfig -> autoconfigure
    Fixes gh-213

--- graphql-dgs-extended-scalars/src/test/kotlin/com/netflix/graphql/dgs/autoconfigure/MultipleRegistrationOfExtendedScalarsTest.kt
+++ graphql-dgs-extended-scalars/src/test/kotlin/com/netflix/graphql/dgs/autoconfigure/MultipleRegistrationOfExtendedScalarsTest.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.netflix.graphql.dgs.autoconfig
+package com.netflix.graphql.dgs.autoconfigure
 
 import com.netflix.graphql.dgs.*
 import graphql.scalars.ExtendedScalars
--- graphql-dgs-spring-boot-oss-autoconfigure/src/test/kotlin/com/netflix/graphql/dgs/autoconfigure/DgsConfigurationPropertiesTest.kt
+++ graphql-dgs-spring-boot-oss-autoconfigure/src/test/kotlin/com/netflix/graphql/dgs/autoconfigure/DgsConfigurationPropertiesTest.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.netflix.graphql.dgs.autoconfig
+package com.netflix.graphql.dgs.autoconfigure
 
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
