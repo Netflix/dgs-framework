ce5896312c1331e8e348422b9f51397e71ea22b4
Paul Bakker <pbakker@netflix.com>
2021-05-11 16:35:48 -0700

    Add graphql-java dependency to client, so it can use Coercing.

--- graphql-dgs-client/build.gradle.kts
+++ graphql-dgs-client/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation("org.springframework:spring-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    implementation("com.graphql-java:graphql-java")
 
     testImplementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.springframework:spring-test")
