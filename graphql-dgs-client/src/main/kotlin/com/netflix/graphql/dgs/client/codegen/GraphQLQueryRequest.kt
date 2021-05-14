3dda120f056e11b935e7138cab2e73c751adbeba
Paul Bakker <paul.bakker.nl@gmail.com>
2021-05-13 17:57:46 -0700

    Scalar in client (#333)
    * Add graphql-java dependency to client, so it can use Coercing.
    
    * Added support to pass in a map of scalars to a GraphQLRequest
    
    * Added support for nested scalars
    
    * ignoring Companion objects
    
    * more testcases for primitives/boxed values
    
    * more testcases for lists
    
    * support getting properties from base classes.
    
    * added test for escape characters in strings
    
    * Calling toString for some special classes for backward comptibiliy
    
    Co-authored-by: Paul Bakker <pbakker@netflix.com>

--- graphql-dgs-client/src/main/kotlin/com/netflix/graphql/dgs/client/codegen/GraphQLQueryRequest.kt
+++ graphql-dgs-client/src/main/kotlin/com/netflix/graphql/dgs/client/codegen/GraphQLQueryRequest.kt
@@ -16,9 +16,21 @@
 
 package com.netflix.graphql.dgs.client.codegen
 
-class GraphQLQueryRequest(private val query: GraphQLQuery, private val projection: BaseProjectionNode?) {
+import graphql.schema.Coercing
+import org.springframework.util.ReflectionUtils
+import java.lang.reflect.Field
+import java.time.*
+import java.util.*
 
-    constructor(query: GraphQLQuery) : this(query, null)
+class GraphQLQueryRequest(
+    private val query: GraphQLQuery,
+    private val projection: BaseProjectionNode?,
+    scalars: Map<Class<*>, Coercing<*, *>>?
+) {
+
+    constructor(query: GraphQLQuery) : this(query, null, null)
+    constructor(query: GraphQLQuery, projection: BaseProjectionNode?) : this(query, projection, null)
+    private val inputValueSerializer = InputValueSerializer(scalars ?: emptyMap())
 
     fun serialize(): String {
         val builder = StringBuilder()
@@ -36,22 +48,7 @@ class GraphQLQueryRequest(private val query: GraphQLQuery, private val projectio
                 if (value != null) {
                     builder.append(key)
                     builder.append(": ")
-                    if (value is String) {
-                        builder.append("\"")
-                        builder.append(value.toString())
-                        builder.append("\"")
-                    } else if (value is List<*>) {
-                        if (value.isNotEmpty() && value[0] is String) {
-                            builder.append("[")
-                            val result = value.joinToString(separator = "\", \"", prefix = "\"", postfix = "\"")
-                            builder.append(result)
-                            builder.append("]")
-                        } else {
-                            builder.append(value.toString())
-                        }
-                    } else {
-                        builder.append(value.toString())
-                    }
+                    builder.append(inputValueSerializer.serialize(value))
                 }
                 if (inputEntryIterator.hasNext()) {
                     builder.append(", ")
@@ -63,10 +60,63 @@ class GraphQLQueryRequest(private val query: GraphQLQuery, private val projectio
         if (projection is BaseSubProjectionNode<*, *>) {
             builder.append(projection.root().toString())
         } else if (projection != null) {
+
             builder.append(projection.toString())
         }
 
         builder.append(" }")
         return builder.toString()
+    }
+}
+
+class InputValueSerializer(private val scalars: Map<Class<*>, Coercing<*, *>> = emptyMap()) {
+    companion object {
+        val toStringClasses = setOf(
+            String::class.java,
+            LocalDateTime::class.java,
+            LocalDate::class.java,
+            LocalTime::class.java,
+            TimeZone::class.java,
+            Date::class.java,
+            OffsetDateTime::class.java,
+            Currency::class.java,
+            Instant::class.java
+        )
+    }
+
+    fun serialize(input: Any?): String? {
+        if (input == null) {
+            return null
+        }
+
+        val type = input::class.java
+
+        return if (scalars.contains(type)) {
+            """"${scalars[type]!!.serialize(input)}""""
+        } else if (type.isPrimitive || type.isAssignableFrom(java.lang.Integer::class.java) || type.isAssignableFrom(java.lang.Long::class.java) || type.isAssignableFrom(java.lang.Double::class.java) || type.isAssignableFrom(java.lang.Float::class.java) || type.isAssignableFrom(java.lang.Boolean::class.java) || type.isAssignableFrom(java.lang.Short::class.java) || type.isAssignableFrom(java.lang.Byte::class.java) || type.isEnum) {
+            input.toString()
+        } else if (toStringClasses.contains(type)) {
+            // Call toString for known types, in case no scalar is found. This is for backward compatibility.
+            """"$input""""
+        } else if (input is List<*>) {
+            """[${input.filterNotNull().joinToString(", ") { listItem -> serialize(listItem) ?: "" }}]"""
+        } else {
+            val fields = LinkedList<Field>()
+            ReflectionUtils.doWithFields(input.javaClass) {
+                ReflectionUtils.makeAccessible(it)
+                fields.add(it)
+            }
+
+            fields.filter { !it.type::class.isCompanion }.mapNotNull {
+                val nestedValue = it.get(input)
+                if (nestedValue != null && nestedValue::class.java == input::class.java) {
+                    """${it.name}:$nestedValue"""
+                } else if (nestedValue != null && !nestedValue::class.isCompanion) {
+                    """${it.name}:${serialize(nestedValue)}"""
+                } else {
+                    null
+                }
+            }.joinToString(", ", "{", " }")
+        }
     }
 }
