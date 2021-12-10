f9cccc211f41d6ed392dae82efd630e54cb842a5
Bernardo Gomez Palacio <bernardo.gomezpalacio@gmail.com>
2021-12-10 14:15:54 -0800

    Upgrade Framework Patch Versions
    We are upgrading the path versions of the following Framework
    Dependencies..
    
    * Kotlin from 1.5.21 to 1.5.32
    * Spring Framework  from 5.2.13.RELEASE to 5.2.18.RELEASE
    * Spring Boot from 2.3.9.RELEASE to 2.3.12.RELEASE
    * Spring Security from 5.3.9.RELEASE to 5.3.12.RELEASE
    * Spring Cloud from HOXTON.SR10 to HOXTON.SR12
    * Jackson from 2.12.3 to 2.12.5

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -15,10 +15,5 @@
  */
 
 object Versions {
-    const val KOTLIN_VERSION = "1.5.21"
-    const val SPRING_VERSION = "5.2.13.RELEASE"
-    const val SPRING_BOOT_VERSION = "2.3.9.RELEASE"
-    const val SPRING_SECURITY_VERSION = "5.3.9.RELEASE"
-    const val SPRING_CLOUD_VERSION = "Hoxton.SR10"
-    const val JACKSON_BOM = "2.12.3"
+    const val KOTLIN_VERSION = "1.5.32"
 }
