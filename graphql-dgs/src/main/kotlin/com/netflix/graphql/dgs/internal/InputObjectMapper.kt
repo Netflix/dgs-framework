eb2545da24654320385ac2051da181c2025d3ae4
Paul Bakker <pbakker@netflix.com>
2022-01-24 14:58:47 -0800

    More javadoc

--- graphql-dgs/src/main/kotlin/com/netflix/graphql/dgs/internal/InputObjectMapper.kt
+++ graphql-dgs/src/main/kotlin/com/netflix/graphql/dgs/internal/InputObjectMapper.kt
@@ -32,17 +32,34 @@ import kotlin.reflect.full.primaryConstructor
 import kotlin.reflect.jvm.javaType
 import kotlin.reflect.jvm.jvmErasure
 
+/**
+ * SPI intended for other frameworks/libraries that need to customize how input objects are mapped.
+ * Not intended to be used by most users.
+ *
+ * A custom mapper might call the DefaultInputObjectMapper, passing itself to the DefaultInputObjectMapper constructor.
+ * The DefaultInputObjectMapper will invoke the custom mapper each time it goes a level deeper into a nested object structure.
+ * This makes it possible to have a custom mapper that still mostly relies on the default one.
+ * Be careful to not create an infinite recursion calling back and forward though!
+ *
+ * Kotlin and Java objects are converted differently. This is to deal with things like data classes.
+ * The input to the map methods is a map of values that are already converted by scalars.
+ * The input IS NOT JSON. Attempting to use a JSON mapper to converting these values will result in incorrect scalar values.
+ */
 interface InputObjectMapper {
     /**
-     * SPI intended for other frameworks/libraries that need to customize how input objects are mapped.
-     * Not intended to be used by most users.
-     *
-     * A custom mapper might call the DefaultInputObjectMapper, passing itself to the DefaultInputObjectMapper constructor.
-     * The DefaultInputObjectMapper will invoke the custom mapper each time it goes a level deeper into a nested object structure.
-     * This makes it possible to have a custom mapper that still mostly relies on the default one.
-     * Be careful to not create an infinite recursion calling back and forward though!
+     * Convert a map of input values to a Kotlin object. This must support Kotlin constructs such as data classes, and is typically handled differently from Java.
+     * @param inputMap The fields for an input object represented as a Map. This can be a nested map if nested types are used. Note that the values in this map are already converted by the scalars representing these types.
+     * @param targetClass The class to convert to.
+     * @return The converted object
      */
     fun <T : Any> mapToKotlinObject(inputMap: Map<String, *>, targetClass: KClass<T>): T
+
+    /**
+     * Convert a map of input values to a Java object.
+     * @param inputMap The fields for an input object represented as a Map. This can be a nested map if nested types are used. Note that the values in this map are already converted by the scalars representing these types.
+     * @param targetClass The class to convert to.
+     * @return The converted object
+     */
     fun <T> mapToJavaObject(inputMap: Map<String, *>, targetClass: Class<T>): T
 }
 
