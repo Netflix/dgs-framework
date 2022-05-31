e344d3b6debfc470e79fe734aa697df53ae343d0
Bernardo Gomez Palacio <bernardo.gomezpalacio@gmail.com>
2022-05-31 12:36:09 -0700

    Adopt Gradle JVM Toolchain
    This commit adopts [Gradle's JVM Toolchain] support such that Gradle
    automatically finds a compatible JDK installed locally.
    In an environment where folks have JDK 8 installed among others, such as 11 or 17,
    this enable Gradle to handle the switch to 8 automatically.
    
    [Gradle's JVM Toolchain]: https://docs.gradle.org/current/userguide/toolchains.html

--- build.gradle.kts
+++ build.gradle.kts
@@ -111,8 +111,9 @@ configure(subprojects.filterNot { it in internalBomModules }) {
     }
 
     java {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(8))
+        }
     }
 
     kapt {
