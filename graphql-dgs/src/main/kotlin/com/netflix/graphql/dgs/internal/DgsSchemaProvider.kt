Netflix/dgs-framework

commit 6fbce099d30b9c5e48f6fd31ebd29e9f91dcb3c2

Fix excess methods leading to schema duplicate in case mocked DataFetcher

graphql-dgs/src/main/kotlin/com/netflix/graphql/dgs/internal/DgsSchemaProvider.kt
@@ -62,6 +62,7 @@ import java.util.*
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.CompletionStage
 import java.util.concurrent.locks.ReentrantReadWriteLock
+import java.util.stream.Collectors
 import kotlin.concurrent.read
 import kotlin.concurrent.write
 
@@ -264,7 +265,12 @@ class DgsSchemaProvider(
                 .forEach { method ->
                     val mergedAnnotations =
                         MergedAnnotations.from(method, MergedAnnotations.SearchStrategy.TYPE_HIERARCHY)
-                    mergedAnnotations.stream(DgsData::class.java).forEach { dgsDataAnnotation ->
+                    val filteredMergedAnnotations =
+                        mergedAnnotations
+                            .stream(DgsData::class.java)
+                            .filter { (it.source as Method).declaringClass == method.declaringClass }
+                            .collect(Collectors.toList())
+                    filteredMergedAnnotations.forEach { dgsDataAnnotation ->
                         registerDataFetcher(
                             typeDefinitionRegistry,
                             codeRegistryBuilder,

commit 30a0d12c643f7f36e31a9a6b8aa361a016057ded

[regresion] Allow Spock Stubs to be used as Data Fetchers
Commit 6fbce099 introduced a regression that will prevent Spock Stubs to
be used as Data Fetchers.

graphql-dgs/src/main/kotlin/com/netflix/graphql/dgs/internal/DgsSchemaProvider.kt
@@ -267,7 +267,7 @@ class DgsSchemaProvider(
                     val filteredMergedAnnotations =
                         mergedAnnotations
                             .stream(DgsData::class.java)
-                            .filter { (it.source as Method).declaringClass == method.declaringClass }
+                            .filter { AopUtils.getTargetClass((it.source as Method).declaringClass) == AopUtils.getTargetClass(method.declaringClass) }
                             .collect(Collectors.toList())
                     filteredMergedAnnotations.forEach { dgsDataAnnotation ->
                         registerDataFetcher(