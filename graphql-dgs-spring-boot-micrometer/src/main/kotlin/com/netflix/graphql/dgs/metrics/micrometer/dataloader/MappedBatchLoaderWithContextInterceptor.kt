af0c51d435fb942ba120fc1020d6e11fcae5324f
ksrinivasan <ksrinivasan@netflix.com>
2023-02-23 13:35:08 -0800

    Add better logging of exceptions setting up data loader timers.

--- graphql-dgs-spring-boot-micrometer/src/main/kotlin/com/netflix/graphql/dgs/metrics/micrometer/dataloader/MappedBatchLoaderWithContextInterceptor.kt
+++ graphql-dgs-spring-boot-micrometer/src/main/kotlin/com/netflix/graphql/dgs/metrics/micrometer/dataloader/MappedBatchLoaderWithContextInterceptor.kt
@@ -34,7 +34,7 @@ internal class MappedBatchLoaderWithContextInterceptor(
                 )
             }
         } catch (exception: Exception) {
-            logger.warn("Error creating timer interceptor '{}' for {}", ID, javaClass.simpleName)
+            logger.warn("Error creating timer interceptor '{}' for {} with exception {}", ID, javaClass.simpleName, exception.message)
             pipe.to(batchLoader)
         }
     }
