1c169b5b50a7932b88f726fa83e7a96417fbc396
ksrinivasan <ksrinivasan@netflix.com>
2023-04-24 14:26:14 -0700

    Pin io.projectreactor.netty:reactor-netty-http to 1.1.5 temporarily, since 1.1.0 is not compatible.

--- graphql-dgs-platform/build.gradle.kts
+++ graphql-dgs-platform/build.gradle.kts
@@ -81,6 +81,9 @@ dependencies {
         api("io.projectreactor:reactor-test"){
             version { require("3.4.22") }
         }
+        api( "io.projectreactor.netty:reactor-netty-http"){
+            version { require("1.1.5") }
+        }
         // CVEs
         api("org.apache.logging.log4j:log4j-to-slf4j:2.19.0") {
             because("Refer to CVE-2021-44228; https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2021-44228")
