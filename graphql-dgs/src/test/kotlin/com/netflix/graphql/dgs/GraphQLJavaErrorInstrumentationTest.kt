cdb80fc34edcaa214037fa3a0df804edd4eb8f71
$(git --no-pager log --format=format:'%an' -n 1) <ksrinivasan@netflix.com>
2024-05-09 12:13:45 -0700

    Fix lint errors.

--- graphql-dgs/src/test/kotlin/com/netflix/graphql/dgs/GraphQLJavaErrorInstrumentationTest.kt
+++ graphql-dgs/src/test/kotlin/com/netflix/graphql/dgs/GraphQLJavaErrorInstrumentationTest.kt
@@ -28,7 +28,7 @@ import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
-class ValidationErrorInstrumentationTest {
+class GraphQLJavaErrorInstrumentationTest {
 
     private lateinit var graphqlJavaErrorInstrumentation: Instrumentation
 
