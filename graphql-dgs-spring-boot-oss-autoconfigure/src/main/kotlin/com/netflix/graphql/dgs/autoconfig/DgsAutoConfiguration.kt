fae5ded1bc15d4163b3198d2598d298e1b1b9e14
Patrick Strawderman <pstrawderman@netflix.com>
2024-07-17 09:33:09 -0700

    Make GraphQLContextContributor a functional interface

--- graphql-dgs-spring-boot-oss-autoconfigure/src/main/kotlin/com/netflix/graphql/dgs/autoconfig/DgsAutoConfiguration.kt
+++ graphql-dgs-spring-boot-oss-autoconfigure/src/main/kotlin/com/netflix/graphql/dgs/autoconfig/DgsAutoConfiguration.kt
@@ -35,7 +35,6 @@ import com.netflix.graphql.dgs.internal.method.ArgumentResolver
 import com.netflix.graphql.dgs.internal.method.MethodDataFetcherFactory
 import com.netflix.graphql.dgs.scalars.UploadScalar
 import com.netflix.graphql.mocking.MockProvider
-import graphql.GraphQLContext
 import graphql.execution.AsyncExecutionStrategy
 import graphql.execution.AsyncSerialExecutionStrategy
 import graphql.execution.DataFetcherExceptionHandler
@@ -49,7 +48,6 @@ import graphql.schema.DataFetcherFactory
 import graphql.schema.GraphQLCodeRegistry
 import graphql.schema.GraphQLSchema
 import graphql.schema.idl.TypeDefinitionRegistry
-import graphql.schema.visibility.DefaultGraphqlFieldVisibility.DEFAULT_FIELD_VISIBILITY
 import graphql.schema.visibility.GraphqlFieldVisibility
 import io.micrometer.context.ContextRegistry
 import io.micrometer.context.ContextSnapshotFactory
@@ -78,7 +76,7 @@ import org.springframework.mock.web.MockHttpServletRequest
 import org.springframework.web.context.request.NativeWebRequest
 import org.springframework.web.context.request.WebRequest
 import java.time.Duration
-import java.util.*
+import java.util.Optional
 import java.util.concurrent.Executors
 import java.util.concurrent.ScheduledExecutorService
 
@@ -282,8 +280,13 @@ open class DgsAutoConfiguration(
 
     @Bean
     @ConditionalOnMissingBean
-    open fun schema(dgsSchemaProvider: DgsSchemaProvider, fieldVisibility: GraphqlFieldVisibility): GraphQLSchema {
-        return dgsSchemaProvider.schema(null, fieldVisibility).graphQLSchema
+    open fun schema(dgsSchemaProvider: DgsSchemaProvider, fieldVisibility: GraphqlFieldVisibility?): GraphQLSchema {
+        val result = if (fieldVisibility == null) {
+            dgsSchemaProvider.schema(schema = null)
+        } else {
+            dgsSchemaProvider.schema(schema = null, fieldVisibility = fieldVisibility)
+        }
+        return result.graphQLSchema
     }
 
     @Bean
@@ -309,21 +312,7 @@ open class DgsAutoConfiguration(
         matchIfMissing = false
     )
     open fun disableIntrospectionContextContributor(): GraphQLContextContributor {
-        return object : GraphQLContextContributor {
-            override fun contribute(
-                builder: GraphQLContext.Builder,
-                extensions: Map<String, Any>?,
-                requestData: DgsRequestData?
-            ) {
-                builder.put(Introspection.INTROSPECTION_DISABLED, true)
-            }
-        }
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    open fun defaultFieldVisibility(): GraphqlFieldVisibility {
-        return DEFAULT_FIELD_VISIBILITY
+        return GraphQLContextContributor { builder, _, _ -> builder.put(Introspection.INTROSPECTION_DISABLED, true) }
     }
 
     @Bean
