0c30d432429f5349c5c205659e3b10fea2af809b
aeonframework <aeonframework@proton.me>
2026-09-02 07:36:55 +0000

    fix(deps): bump Spring Boot BOM to patch CVE-2026-41854
    Advisory: https://github.com/advisories/GHSA-7m2p-62gw-p8qq
    Severity: moderate
    Fixed in: org.springframework:spring-web 7.0.8 (via spring-boot-dependencies 4.0.8)

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,7 +51,7 @@ allprojects {
     // and suggest an upgrade. The only exception currently are those defined
     // in buildSrc, most likely because the variables are used in plugins as well
     // as dependencies. e.g. KOTLIN_VERSION
-    extra["sb.version"] = "4.0.0"
+    extra["sb.version"] = "4.0.8"
     extra["kotlin.version"] = Versions.KOTLIN_VERSION
     extra["json-path.version"] = "3.0.0"
 }
